3b57cf438aab0ddd4efce944c1e27c4fe5bea413
Ryan Lin <43688324+ryanlin@users.noreply.github.com>
2018-12-10 14:25:18 -0800

    Update LeftLeaningRedBlackTree.h

--- LeftLeaningRedBlackTree.h
+++ LeftLeaningRedBlackTree.h
@@ -10,16 +10,22 @@ private:
 	RedBlackNode<ItemType>* mroot;
 
 protected:
-
+	int getNumberOfNodesHelper(BinaryNode<ItemType>* root) const;
 public:
 //Constructors and Deconstructors
 	LeftLeaningRedBlackTree() : mroot(NULL) { }
 	LeftLeaningRedBlackTree(RedBlackNode<ItemType>& root);
 	LeftLeaningRedBlackTree(const LeftLeaningRedBlackTree<ItemType>& src_tree);
-	~LeftLeaningRedBlackTree() {/*blank for now*/}
+	~LeftLeaningRedBlackTree();
 //Getters abd Element Access
-	bool isEmpty() const { return mroot == NULL; }
-	int getHeight() const;
+	bool isEmpty() const; 
+	int getNumberOfNodes() const;
+  RedBlackNode& getRootAddress() const;
+  ItemType getRootData() const;
+  
+  void RotateLeft();
+  void RotateRight();
+  
 //?	RedBlackNode& getEntry(int index);
 //?	ItemType getItem{int index;
 //Modifiers
@@ -28,3 +34,54 @@ public:
 	void clear();
 	
 };
+
+//Constructors and Deconstructors
+template<typename ItemType>
+LeftLeaningRedBlackTree<ItemType>::LeftLeaningRedBlackTree(RedBlackNode<ItemType>& root)
+	: mroot(new RedBlackNode<ItemType>(root) )
+{
+//blank
+}
+template<typename ItemType>
+LeftLeaningRedBlackTree<ItemType>::LeftLeaningRedBlackTree(const LeftLeaningRedBlackTree<ItemType>& src_tree)
+{
+
+}
+template<typename ItemType>
+LeftLeaningRedBlackTree<ItemType>::~LeftLeaningRedBlackTree()
+{
+	/*blank for now*/
+}
+
+//
+
+template<typename ItemType>
+bool LeftLeaningRedBlackTree<ItemType>::isEmpty() const
+{
+	return mroot == NULL;
+}
+template<typename ItemType>
+int LeftLeaningRedBlackTree<ItemType>::getNumberOfNodes() const
+{
+	return getNumberOfNodesHelper(mroot);
+}
+
+
+//Protected and Helper Functions
+template<typename ItemType>
+int LeftLeaningRedBlackTree<ItemType>::getNumberOfNodesHelper(BinaryNode<ItemType>* root) const
+{
+	if( root == NULL )
+	{
+		return 0;
+	}
+	else if( root->isLeaf() )
+	{
+		return 1;
+	}
+	else
+	{
+		getNumberOfNodesHelper(root->getLeftChildPtr()) + getNumberOfNodesHelper(root->getRightChildPtr()) + 1;
+	}
+}
+
